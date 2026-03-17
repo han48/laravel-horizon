889d22b2fef0adb4a07b22d70c2b69967b12e280
han48 <zzz4everzzz@live.co.uk>
2026-03-17 09:30:57 +0700

    Update Horizon configuration for Telescope integration

--- config/horizon.php
+++ config/horizon.php
@@ -6,6 +6,19 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    | Telescope Master Switch
+    |--------------------------------------------------------------------------
+    |
+    | This option may be used to disable all Telescope watchers regardless
+    | of their individual configuration, which simply provides a single
+    | and convenient way to enable or disable Telescope data storage.
+    |
+    */
+
+    'enabled' => env('HORIZON_ENABLED', true),
+
+    /*
+    |--------------------------------------------------------------------------
     | Horizon Name
     |--------------------------------------------------------------------------
     |
